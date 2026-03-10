78389b3a2faa79f50f169aa42d0e0a915364d68a
Trevor Henry <trevorhenry20@gmail.com>
2026-03-10 13:04:03 +0000

    File changes

--- src/components/chambers/chimePolyfill.jsx
+++
@@ -1,7 +0,0 @@
-// Polyfill required by amazon-chime-sdk-js which expects Node.js globals in browser
-if (typeof global === 'undefined') {
-  window.global = window;
-}
-if (typeof process === 'undefined') {
-  window.process = { env: {}, browser: true };
-}
